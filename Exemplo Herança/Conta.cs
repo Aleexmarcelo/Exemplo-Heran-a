8a3aafa55345e1050e4fdd14855ca021a58ea39d
Aleexmarcelo <jojoanete@gmail.com>
2021-10-13 21:48:58 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo Herança/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo_Herança
+{
+    public class Conta
+    {
+        //Variáveis com encapsulamento
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; protected set; }
+        //Construtor comum
+        public Conta()
+        {
+        }
+        //Construtor personalizado
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        //Métodos de Saque e Depósito
+        public void Saque(double saldoTotal)
+        {
+            Saldo -= saldoTotal;
+        }
+        public void Deposito(double saldoTotal)
+        {
+            Saldo += saldoTotal;
+        }
+    }
+}
---
+++ Exemplo Herança/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exemplo_Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrar com dados da conta com nome, numero e saldo
+            Console.WriteLine("Envie o seu número da conta: ");
+            int Numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Envie o seu nome: ");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Envie o seu saldo atual: ");
+            double Saldo = int.Parse(Console.ReadLine());
+
+            //Cria a conta juridica com os dados digitados
+            ContaJuridica conta = new ContaJuridica(Numero, Nome, Saldo, 500.0);
+
+            //Printa o saldo atual da conta
+            Console.WriteLine("Seu saldo atual é de: " + "R$" + conta.Saldo);
+        }
+    }
+}
